cc52068aa1af5b527bc41097142400430a6c1088
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-16 15:15:21 -0200

    declaração de char e string - aula 4

---
+++ 1-primeiros passos/AprendendoCSharp/CharEString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharEString{
+    class Program{
+        static void Main(string[] args){
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) 65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra += (char) 1;
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Curso de C#";
+            Console.WriteLine(titulo);
+
+            string cursos = @"
+-.NET
+-JAVA
+-JavaScript";
+            Console.WriteLine(cursos);
+
+
+            Console.ReadLine();
+        }
+    }
+}
