18a0b1531b3f3282307d848313cf72e5e38bb8c7
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-16 13:20:31 -0200

    Outros tipos numericos e casting - aula 3

---
+++ 1-primeiros passos/AprendendoCSharp/ConversaoEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversaoEOutrosTiposNumericos{
+    class Program{
+        static void Main(string[] args){
+
+            double salario = 1250.50;
+            int salarioInteiro = (int) salario;
+            Console.WriteLine(salarioInteiro);
+
+            //outros tipos de inteiro.
+            long idadeDaTerra = 13000000000;
+
+            short quantidadeProduto = 1500;
+
+            //outro tipo de ponto flutuante.
+            float altura = 1.80f;
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1-primeiros passos/AprendendoCSharp/CriandoPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoPontoFlutuante{
+    class Program{
+        static void Main(string[] args){
+
+            double salario = 1250.50;
+            Console.WriteLine("Meu salário é " + salario);
+
+            salario = 2501 / 2;
+            Console.WriteLine("Este é o meu salário: " + salario);
+
+            salario = 2501.0 / 2;
+            Console.WriteLine("Não! meu salário é " + salario + " reais.");            
+
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
