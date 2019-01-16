bebf1c2aa5237d074f126a9bc68102bb6d1ddb8f
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-16 12:17:53 -0200

    Criando variaveis inteiras.

---
+++ 1-primeiros passos/AprendendoCSharp/CriandoVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoVariaveis{
+    class Program{
+        static void Main(string[] args){
+
+            int idade = 23;
+            Console.WriteLine("Hoje você tem " + idade + " anos.");
+
+            idade += 5;
+            Console.WriteLine("Daqui a 5 anos você terá " + idade + " anos.");
+
+            Console.ReadLine();
+        }
+    }
+}
