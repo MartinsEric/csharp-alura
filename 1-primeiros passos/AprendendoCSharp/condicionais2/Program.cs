707341d66e4cb83cb001771fd41e0b8b960a88a4
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-16 16:00:00 -0200

    Controle de fluxo - usando variavel bool - aula 5

---
+++ 1-primeiros passos/AprendendoCSharp/condicionais2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 18, qtdPessoas = 1;
+            bool acompanhado = qtdPessoas >= 2;
+
+            if (idade >= 18 && acompanhado)
+            {
+                Console.WriteLine("Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
