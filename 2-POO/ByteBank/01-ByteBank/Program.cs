89dd10b91d78a82d30e27849de42a5dc4a5e4a07
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-17 13:25:49 -0200

    Criando instancias

--- 2-POO/ByteBank/01-ByteBank/Program.cs
+++ 2-POO/ByteBank/01-ByteBank/Program.cs
@@ -10,7 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.saldo = 200;
+            Console.WriteLine(contaCorrente.saldo);
+
+            contaCorrente.saldo += 100;
+            Console.WriteLine(contaCorrente.saldo);
+
+            ContaCorrente outraConta = new ContaCorrente();
+
+            outraConta.saldo = 50;
+
+            Console.WriteLine("Saldo da primeira conta: R$" + contaCorrente.saldo);
+            Console.WriteLine("Saldo da segunda conta: R$" + outraConta.saldo);
 
 
             Console.ReadLine();
