7e424c36a114215b27f3782c04a9b636c6e0dfe4
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-01-21 19:21:49 -0200

    DivideByZeroException e NullReferenceException

--- 4-POO - exceções/ByteBank/Program.cs
+++ 4-POO - exceções/ByteBank/Program.cs
@@ -10,7 +10,43 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+            }
+            
             Console.ReadLine();
+        }
+
+        public static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }catch(NullReferenceException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        public static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+
+            return numero / divisor;
         }
     }
 }
