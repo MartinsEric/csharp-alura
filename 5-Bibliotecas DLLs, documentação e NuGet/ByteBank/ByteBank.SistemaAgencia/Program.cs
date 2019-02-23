4aeede40e5af0923f86d14eca2ecca7d99b2ae3d
Eric Martins <ericmartins.martins@gmail.com>
2019-02-23 11:56:01 -0300

    Aula 5 - NuGet, DateTime, TimeSpan e Humanizer

--- 5-Bibliotecas DLLs, documentação e NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 5-Bibliotecas DLLs, documentação e NuGet/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,13 +13,14 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
+            DateTime dataDoisAnos = new DateTime(2019, 3, 22);
+            DateTime dataCorrente = DateTime.Now;
 
+            TimeSpan tempoQueFalta = dataDoisAnos - dataCorrente;
 
-            FuncionarioAutenticavel carlos = new GerenteDeConta("12345678909");
-            carlos.Autenticar("kjhasfkjas");
+            string mensagem = "faltam " + TimeSpanHumanizeExtensions.Humanize(tempoQueFalta) + " Para completar 2 anos de namoro com o amor da sua vida.";
 
-            Console.WriteLine(conta.Numero);
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
