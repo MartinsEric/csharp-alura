4751281cc275ecec2576213f2ae75db7aa0cf61d
unknown <suporte@TJENLA02E23.LABUVA.EDU>
2019-03-11 18:00:50 -0300

    Aula 5 - Sobrepondo ToString e Equals

--- 6 - String, Expressões regulares e Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 6 - String, Expressões regulares e Classe Object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -14,6 +14,36 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            //Sobrepondo ToString e Equals
+            ContaCorrente conta = new ContaCorrente(123, 123456);
+
+            Console.WriteLine(conta);
+
+            Cliente eric = new Cliente();
+            eric.Nome = "Eric Martins";
+            eric.CPF = "123456789-98";
+            eric.Profissao = "Desenvolvedor";
+
+            Cliente eric_2 = new Cliente();
+            eric_2.Nome = "Eric Martins";
+            eric_2.CPF = "123456789-98";
+            eric_2.Profissao = "Desenvolvedor";
+
+            if (eric.Equals(eric_2))
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("São diferentes!");
+            }
+
+            Console.ReadLine();
+            
+        }
+
+        public static void ExpressaoRegular()
+        {
             //Teste de expressao regular.
             string expReg = "[0-9]{4,5}[- ]?[0-9]{4}";
             string textoTeste = "anote o telefone 8543-2134";
@@ -23,18 +53,20 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(resultado);
 
             Console.ReadLine();
+        }
+
+        public static void ManupulacaoString()
+        {
+            string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
+
+            Console.WriteLine("moeda origem: " + extrator.GetValor("moedaorigem"));
+            Console.WriteLine("moeda destino: " + extrator.GetValor("moedaDeStino"));
+            Console.WriteLine("valor: " + extrator.GetValor("VALOR"));
 
 
-            //string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
-
-            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
-
-            //Console.WriteLine("moeda origem: " + extrator.GetValor("moedaorigem"));
-            //Console.WriteLine("moeda destino: " + extrator.GetValor("moedaDeStino"));
-            //Console.WriteLine("valor: " + extrator.GetValor("VALOR"));
-
-
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
